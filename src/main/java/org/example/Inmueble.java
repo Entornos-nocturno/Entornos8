3f3f6888e393fd8ec27c4a07569fa54fed9a050d
Egoi <egoi.sanchez@ikasle.egibide.org>
2024-04-19 21:38:05 +0200

    primer commit

---
+++ src/main/java/org/example/Inmueble.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Inmueble {
+
+
+
+        private String  nombre;
+        private String direccion;
+        private String precio;
+        private String metros;
+        private String habitaciones;
+        private String Año;
+    }
+
+
