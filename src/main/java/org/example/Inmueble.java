d9f524f7978fc9c873f34c650145f27061cb84cf
davidmerchan <david.merchan@ikasle.egibide.org>
2024-04-19 21:46:26 +0200

    getter and setter

--- src/main/java/org/example/Inmueble.java
+++ src/main/java/org/example/Inmueble.java
@@ -10,6 +10,58 @@ public class Inmueble {
         private String metros;
         private String habitaciones;
         private String Año;
-    }
+
+        public String getNombre() {
+                return nombre;
+        }
+
+        public void setNombre(String nombre) {
+                this.nombre = nombre;
+        }
+
+        public String getDireccion() {
+                return direccion;
+        }
+
+        public void setDireccion(String direccion) {
+                this.direccion = direccion;
+        }
+
+        public String getPrecio() {
+                return precio;
+        }
+
+        public void setPrecio(String precio) {
+                this.precio = precio;
+        }
+
+        public String getMetros() {
+                return metros;
+        }
+
+        public void setMetros(String metros) {
+                this.metros = metros;
+        }
+
+        public String getHabitaciones() {
+                return habitaciones;
+        }
+
+        public void setHabitaciones(String habitaciones) {
+                this.habitaciones = habitaciones;
+        }
+
+        public String getAño() {
+                return Año;
+        }
+
+        public void setAño(String año) {
+                Año = año;
+        }
+}
+
+
+
+
 
 
